ae3ecc2a5a6ac4deaca2f2334cd4b5aa393de864
lzb1988@vip.qq.com <lzb1988>
2018-01-23 16:56:46 +0800

    配置信息

--- docker-entrypoint-initdb.d/init-user-db.sh
+++ docker-entrypoint-initdb.d/init-user-db.sh
@@ -2,10 +2,6 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     -- gitlab
     CREATE USER gitlab PASSWORD 'gitlab';
     CREATE DATABASE gitlab;
+	CREATE EXTENSION pg_trgm;
     GRANT ALL PRIVILEGES ON DATABASE gitlab TO gitlab;
-    -- Mattermost
-    -- https://github.com/mattermost/mattermost-docker
-    CREATE USER mattermost PASSWORD 'mattermost';
-    CREATE DATABASE mattermost;
-    GRANT ALL PRIVILEGES ON DATABASE mattermost TO mattermost;
 EOSQL
